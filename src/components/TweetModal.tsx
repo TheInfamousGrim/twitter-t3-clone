6b440491bdc94d291f45ebb64c5617015de045f0
TheInfamousGrim <finchergeorge1@gmail.com>
2023-03-27 10:31:00 +0100

    Style 💄: Edit tweet stylings

--- src/components/TweetModal.tsx
+++ src/components/TweetModal.tsx
@@ -7,7 +7,10 @@ import Image from 'next/image';
 
 // Icons
 import { XMarkIcon, GlobeEuropeAfricaIcon } from '@heroicons/react/24/solid';
-import { FaceSmileIcon } from '@heroicons/react/24/outline';
+import {
+  FaceSmileIcon,
+  ExclamationTriangleIcon,
+} from '@heroicons/react/24/outline';
 
 // Components
 import { CharacterCounter } from './CharacterCounter';
@@ -92,9 +95,9 @@ export const TweetModal = ({
                         <p className="sr-only">Close</p>
                       </button>
                     </div>
-                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                      <CheckIcon
-                        className="h-6 w-6 text-green-600"
+                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
+                      <ExclamationTriangleIcon
+                        className="h-6 w-6 text-red-600"
                         aria-hidden="true"
                       />
                     </div>
@@ -103,7 +106,7 @@ export const TweetModal = ({
                         as="h3"
                         className="text-base font-semibold leading-6 text-gray-900"
                       >
-                        Payment successful
+                        Looks Like Something went wrong
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
@@ -116,7 +119,7 @@ export const TweetModal = ({
                   <div className="mt-5 sm:mt-6">
                     <button
                       type="button"
-                      className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      className="inline-flex w-full justify-center rounded-md bg-bright-pink px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       onClick={() => setTweetModalOpen(false)}
                     >
                       Go back to dashboard
