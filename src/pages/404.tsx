4349659b50149e901a57e330f20300f156616341
TheInfamousGrim <finchergeorge1@gmail.com>
2023-03-28 09:30:51 +0100

    Feat 🎯: added custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,34 +7,20 @@ import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 dayjs.extend(customParseFormat);
 
-// Types
-import type { GetStaticProps, NextPage } from 'next';
-
-// Helpers
-import { generateSSGHelper } from '~/server/helpers/ssgHelper';
-
-// API
-import { api, type RouterOutputs } from '~/utils/api';
-
 // Components
 import { AuthFooter } from '~/components/AuthFooter';
 import { LoadingPage, LoadingSpinner } from '~/components/LoadingSpinner';
 import { SideNavigation } from '~/components/SideNavigation';
 import { NewToTwooter } from '~/components/NewToTwooter';
 import { PageLayout } from '~/components/PageLayout';
-import { TweetView } from '~/components/TweetView';
 
-const Twoot: NextPage<{ id: string }> = ({ id }) => {
-  const { isLoaded: userLoaded, isSignedIn } = useUser();
-  const { data } = api.tweet.getById.useQuery({
-    id,
-  });
-  if (!data) return <div>404</div>;
+export default function Custom404() {
+  const { isSignedIn } = useUser();
 
   return (
     <>
       <Head>
-        <title>{`Twooter | Twoot - ${data.tweet.id}`}</title>
+        <title>{`Twooter | 404`}</title>
         <meta
           name="description"
           content="A delightful twitter clone made using the awesome t3 stack"
@@ -44,34 +30,19 @@ const Twoot: NextPage<{ id: string }> = ({ id }) => {
       <div className="flex h-full justify-center">
         <SideNavigation />
         <PageLayout>
-          <TweetView {...data} />
+          <div className="flex flex-col items-center justify-center gap-6 p-6 text-center">
+            <h3>{`Hmmm... this page doesn't exist in this dimension. Try heading back to the comfort of home.`}</h3>
+            <Link
+              href="/"
+              className="rounded-full bg-bright-pink py-2.5 px-3.5 text-base font-bold text-white shadow-sm hover:bg-pink-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bright-pink"
+            >
+              Home
+            </Link>
+          </div>
         </PageLayout>
         {!isSignedIn && <NewToTwooter />}
       </div>
       {!isSignedIn && <AuthFooter />}
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = generateSSGHelper();
-
-  const id = context.params?.id;
-
-  if (typeof id !== 'string') throw new Error('no id');
-
-  await ssg.tweet.getById.prefetch({ id });
-
-  return {
-    props: {
-      trpcState: ssg.dehydrate(),
-      id,
-    },
-  };
-};
-
-export const getStaticPaths = () => {
-  return { paths: [], fallback: 'blocking' };
-};
-
-export default Twoot;
+}
